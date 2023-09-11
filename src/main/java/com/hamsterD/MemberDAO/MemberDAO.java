f72a9944c0b1e0bc83f16b7c2d4ac0db62ebb606
geummiri <127584256+geummiri@users.noreply.github.com>
2023-09-11 15:44:11 +0900

    ++Member 객체 변명
    ...

--- src/main/java/com/hamsterD/MemberDAO/MemberDAO.java
+++ src/main/java/com/hamsterD/MemberDAO/MemberDAO.java
@@ -1,10 +1,10 @@
 package com.hamsterD.MemberDAO;
 
-import com.hamsterD.MemberVo.Model;
+import com.hamsterD.MemberVo.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
     @Repository
-public interface MemberDAO extends JpaRepository< Model, Integer > {
+public interface MemberDAO extends JpaRepository<Member, Integer > {
 
 }
