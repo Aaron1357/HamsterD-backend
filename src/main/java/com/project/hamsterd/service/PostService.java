d70b371d859c869e754791efa52f66378e624ff6
geummiri <127584256+geummiri@users.noreply.github.com>
2023-09-11 14:50:44 +0900

    <POST controller 수정>

--- src/main/java/com/project/hamsterd/service/PostService.java
+++ src/main/java/com/project/hamsterd/service/PostService.java
@@ -1,11 +1,38 @@
 package com.project.hamsterd.service;
 
+import com.project.hamsterd.domain.Post;
 import com.project.hamsterd.repo.PostDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
     @Autowired
     private PostDAO dao;
+
+    public List<Post> showAll() {
+        return dao.findAll();
+    }
+
+    public Post show(int postNo) {
+        return dao.findById(postNo).orElse(null);
+    }
+
+    public Post create(Post post) {
+        return dao.save(post);
+    }
+
+    public Post update(Post post) {
+        return dao.save(post);
+    }
+
+    public Post delete(int postNo){
+        Post data = dao.findById(postNo).orElse(null);
+        dao.delete(data);
+        return data;
+    }
+
+
 }
