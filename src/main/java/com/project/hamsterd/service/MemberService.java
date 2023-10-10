67962acd5ae3111743f7df490154c1bf6f2e753a
yoonjongbin <133620288+yoonjongbin@users.noreply.github.com>
2023-10-10 20:07:13 +0900

    BCryptPasswordEncoder 추가못함

--- src/main/java/com/project/hamsterd/service/MemberService.java
+++ src/main/java/com/project/hamsterd/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.project.hamsterd.service;
 
+import com.project.hamsterd.SecurityConfig;
 import com.project.hamsterd.repo.MemberDAO;
 import com.project.hamsterd.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +15,31 @@ public class MemberService {
     @Autowired
     private MemberDAO dao;
 
+    private SecurityConfig security;
+
+
     public List<Member> showAll() {
-     return dao.findAll();
+        return dao.findAll();
     }
 
-    public Member show(int id){
-        return dao.findById(id).orElse(null);
+    public Member show(String id, String password){
+        Member mem = dao.findByMemberId(id);
+
+        if(mem.getId().equals(id) && mem.getPassword().equals(password)){
+            return mem;
+        }
+
+        return null;
 
     }
     public Member create(Member member) {
+
+
+        String encodedPw = security.passwordEncoder().encode(member.getPassword());
+
+        System.out.println("암호화 된 비번 : " + encodedPw);
+        member.setPassword(encodedPw);
+
         return dao.save(member);
     }
 
@@ -30,13 +47,13 @@ public class MemberService {
         Member target = dao.findById(member.getMemberNo()).orElse(null);
 
         if (target != null) return dao.save(member);
-       return null;
+        return null;
     }
 
-   public Member delete(int id) {
+    public Member delete(int id) {
         Member target = dao.findById(id).orElse(null);
         dao.delete(target);
         return target;
-   }
+    }
 
 }
